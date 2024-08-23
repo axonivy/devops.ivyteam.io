6dd2d7c34a4dc70c8222e9f30db2fbc964b78a39
Louis <louis.mueller@axonivy.com>
2024-08-23 09:57:53 +0200

    webhook org check

--- src/main/java/io/ivyteam/devops/github/webhook/GitHubWebhookController.java
+++ src/main/java/io/ivyteam/devops/github/webhook/GitHubWebhookController.java
@@ -1,7 +1,6 @@
 package io.ivyteam.devops.github.webhook;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Conditional;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,10 +13,10 @@ import io.ivyteam.devops.branch.Branch;
 import io.ivyteam.devops.branch.BranchRepository;
 import io.ivyteam.devops.pullrequest.PullRequest;
 import io.ivyteam.devops.pullrequest.PullRequestRepository;
+import io.ivyteam.devops.settings.SettingsManager;
 
 @RestController
 @RequestMapping("/github-webhook/")
-@Conditional(GitHubWebhookCondition.class)
 public class GitHubWebhookController {
 
   @Autowired
@@ -33,6 +32,7 @@ public class GitHubWebhookController {
 
   @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE, headers = "X-GitHub-Event=create")
   public ResponseEntity<Branch> createBranch(@RequestBody BranchBean bean) {
+    validateBean(bean);
     if ("branch".equals(bean.ref_type)) {
       var branch = bean.toBranch();
       branches.create(branch);
@@ -43,6 +43,7 @@ public class GitHubWebhookController {
 
   @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE, headers = "X-GitHub-Event=delete")
   public ResponseEntity<Branch> deleteBranch(@RequestBody BranchBean bean) {
+    validateBean(bean);
     if ("branch".equals(bean.ref_type)) {
       var branch = bean.toBranch();
       branches.delete(branch);
@@ -53,6 +54,7 @@ public class GitHubWebhookController {
 
   @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE, headers = "X-GitHub-Event=pull_request")
   public ResponseEntity<PullRequest> createPR(@RequestBody PullRequestBean bean) {
+    validateBean(bean.organization);
     var pr = bean.toPullRequest();
     if ("opened".equals(bean.action)) {
       prs.create(pr);
@@ -65,10 +67,45 @@ public class GitHubWebhookController {
     return ResponseEntity.notFound().build();
   }
 
-  record BranchBean(String ref, String ref_type, Repository repository, User sender) {
+  private void validateBean(Object bean) {
+    switch (bean) {
+      case BranchBean b -> validateOrg(b.organization);
+      case PullRequestBean pr -> validateOrg(pr.organization);
+      default -> throw new RuntimeException("Invalid request body provided: " + bean);
+    }
+  }
+
+  private void validateOrg(Organization org) {
+    if (org == null || org.login == null || !org.login.equals(SettingsManager.INSTANCE.get().gitHubOrg())) {
+      throw new RuntimeException("Invalid organization provided: " + org);
+    }
+  }
+
+  record BranchBean(
+      String ref,
+      String ref_type,
+      Repository repository,
+      User sender,
+      Organization organization) {
+
     Branch toBranch() {
       return new Branch(this.repository.full_name, this.ref, this.sender.login);
     }
+  }
+
+  record PullRequestBean(
+      String action,
+      PrDetail pull_request,
+      Repository repository,
+      Organization organization) {
+
+    PullRequest toPullRequest() {
+      return new PullRequest(this.repository.full_name, this.pull_request.number, this.pull_request.title,
+          this.pull_request.user.login);
+    }
+  }
+
+  record Organization(String login) {
   }
 
   record Repository(String full_name) {
@@ -76,13 +113,6 @@ public class GitHubWebhookController {
 
   record User(String login) {
 
-  }
-
-  record PullRequestBean(String action, PrDetail pull_request, Repository repository) {
-    PullRequest toPullRequest() {
-      return new PullRequest(this.repository.full_name, this.pull_request.number, this.pull_request.title,
-          this.pull_request.user.login);
-    }
   }
 
   record PrDetail(long number, String title, User user) {
