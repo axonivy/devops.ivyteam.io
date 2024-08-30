7c4d8f1b24417506ae1c059ce89a2dfbb72020cc
Alexander Suter <alexander.suter@axonivy.com>
2024-08-30 12:04:04 +0200

    Access token

--- src/main/java/io/ivyteam/devops/security/SecurityConfiguration.java
+++ src/main/java/io/ivyteam/devops/security/SecurityConfiguration.java
@@ -1,8 +1,15 @@
 package io.ivyteam.devops.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.AuthorizedClientServiceOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 
 import com.vaadin.flow.spring.security.VaadinWebSecurity;
 
@@ -14,5 +21,20 @@ public class SecurityConfiguration extends VaadinWebSecurity {
   protected void configure(HttpSecurity http) throws Exception {
     super.configure(http);
     http.oauth2Login(c -> c.loginPage("/login").permitAll());
+  }
+
+  @Bean
+  public OAuth2AuthorizedClientManager authorizedClientManager(
+      ClientRegistrationRepository clientRegistrationRepository,
+      OAuth2AuthorizedClientService authorizedClientService) {
+
+    OAuth2AuthorizedClientProvider authorizedClientProvider = OAuth2AuthorizedClientProviderBuilder.builder()
+        .clientCredentials().build();
+
+    AuthorizedClientServiceOAuth2AuthorizedClientManager authorizedClientManager = new AuthorizedClientServiceOAuth2AuthorizedClientManager(
+        clientRegistrationRepository, authorizedClientService);
+    authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
+
+    return authorizedClientManager;
   }
 }
