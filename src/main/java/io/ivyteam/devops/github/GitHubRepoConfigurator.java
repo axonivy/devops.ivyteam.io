ea4a884090e9b92720709dd73545205bcc2a6250
Alexander Suter <alexander.suter@axonivy.com>
2024-09-03 09:00:33 +0200

    Add logs to settings synchronizer

--- src/main/java/io/ivyteam/devops/github/GitHubRepoConfigurator.java
+++ src/main/java/io/ivyteam/devops/github/GitHubRepoConfigurator.java
@@ -2,12 +2,17 @@ package io.ivyteam.devops.github;
 
 import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import io.ivyteam.devops.branch.Branch;
 import io.ivyteam.devops.branch.BranchRepository;
 import io.ivyteam.devops.repo.Repo;
 import io.ivyteam.devops.settings.SettingsManager;
 
 public class GitHubRepoConfigurator {
+
+  private final static Logger LOGGER = LoggerFactory.getLogger(GitHubRepoConfigurator.class);
 
   private final GitHubProvider gitHub;
   private final BranchRepository branches;
@@ -21,30 +26,41 @@ public class GitHubRepoConfigurator {
 
   public boolean run() {
     try {
+      LOGGER.info("Update settings of " + repo.name());
       var changed = false;
       var ghRepo = gitHub.get().getRepository(repo.name());
-      if (ghRepo.isPrivate() || ghRepo.isArchived()) {
+      if (ghRepo.isPrivate()) {
+        LOGGER.info("Skip " + repo.name() + " because it is private");
+        return changed;
+      }
+      if (ghRepo.isArchived()) {
+        LOGGER.info("Skip " + repo.name() + " because it is archived");
         return changed;
       }
       var brs = branches.findByRepo(repo.name());
       if (!repo.deleteBranchOnMerge()) {
+        LOGGER.info("Set delete branch on merge to true");
         ghRepo.deleteBranchOnMerge(true);
         changed = true;
       }
       if (repo.projects()) {
+        LOGGER.info("Disable projects");
         ghRepo.enableProjects(false);
         changed = true;
       }
       if (!repo.issues()) {
+        LOGGER.info("Enable issues");
         ghRepo.enableIssueTracker(true);
         changed = true;
       }
       if (repo.wiki()) {
+        LOGGER.info("Disable wiki");
         ghRepo.enableWiki(false);
         changed = true;
       }
       if (repo.hooks()) {
         for (var hook : ghRepo.getHooks()) {
+          LOGGER.info("Delete hook");
           hook.delete();
           changed = true;
         }
@@ -53,6 +69,7 @@ public class GitHubRepoConfigurator {
         if (needsProtection(br)) {
           if (!br.protectedBranch()) {
             var ghBranch = ghRepo.getBranch(br.name());
+            LOGGER.info("Enable protection");
             ghBranch.enableProtection()
                 .requiredReviewers(0)
                 .includeAdmins()
@@ -62,6 +79,7 @@ public class GitHubRepoConfigurator {
         } else {
           if (br.protectedBranch()) {
             var ghBranch = ghRepo.getBranch(br.name());
+            LOGGER.info("Disable protection");
             ghBranch.disableProtection();
             changed = true;
           }
@@ -75,6 +93,7 @@ public class GitHubRepoConfigurator {
 
   private boolean needsProtection(Branch branch) {
     for (var prefix : SettingsManager.INSTANCE.get().branchProtectionPrefixes().split(",")) {
+      LOGGER.info("Check if " + branch.name() + " starts with " + prefix);
       if (branch.name().startsWith(prefix)) {
         return false;
       }
