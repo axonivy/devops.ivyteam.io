f6cf00714a50e64604bd7c51dc698302d1dce6c1
Alexander Suter <alexander.suter@axonivy.com>
2024-12-23 09:48:33 +0100

    Add more tests

--- src/test/java/io/ivyteam/devops/pullrequest/TestPullRequestRepository.java
+++ src/test/java/io/ivyteam/devops/pullrequest/TestPullRequestRepository.java
@@ -3,6 +3,7 @@ package io.ivyteam.devops.pullrequest;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.nio.file.Path;
+import java.util.Map;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -22,11 +23,21 @@ class TestPullRequestRepository {
 
   PullRequestRepository prs;
 
+  PullRequest PR = PullRequest
+      .create()
+      .title("PR-1")
+      .user("alex")
+      .repository("axonivy/test")
+      .id(1)
+      .branchName("master")
+      .build();
+
   @BeforeEach
   void beforeEach() {
     var db = new Database(tempDir.resolve("test.db"));
     var users = new UserRepository(db);
     repos = new RepoRepository(db);
+    repos.create(Repo.create().name("axonivy/test").build());
     prs = new PullRequestRepository(db, users);
   }
 
@@ -36,18 +47,43 @@ class TestPullRequestRepository {
   }
 
   @Test
-  void create() {
-    repos.create(Repo.create().name("axonivy/test").build());
-    var pr = PullRequest
-        .create()
-        .title("PR-1")
-        .user("alex")
-        .repository("axonivy/test")
-        .id(1)
-        .branchName("master")
-        .build();
+  void findByUser_empty() {
+    assertThat(prs.findByUser("suter")).isEmpty();
+  }
 
-    prs.create(pr);
-    assertThat(prs.all()).containsExactly(pr);
+  @Test
+  void findByUser() {
+    prs.create(PR);
+    assertThat(prs.findByUser("alex")).containsExactly(PR);
+  }
+
+  @Test
+  void findByRepository_empty() {
+    assertThat(prs.findByRepository("gotham")).isEmpty();
+  }
+
+  @Test
+  void findByRepository() {
+    prs.create(PR);
+    assertThat(prs.findByRepository("axonivy/test")).containsExactly(PR);
+  }
+
+  @Test
+  void delete() {
+    prs.create(PR);
+    prs.delete(PR);
+    assertThat(prs.all()).isEmpty();
+  }
+
+  @Test
+  void countByRepo() {
+    prs.create(PR);
+    assertThat(prs.countByRepo()).containsExactly(Map.entry("axonivy/test", 1L));
+  }
+
+  @Test
+  void create() {
+    prs.create(PR);
+    assertThat(prs.all()).containsExactly(PR);
   }
 }
